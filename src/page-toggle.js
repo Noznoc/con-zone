50c9f242b3b29172f030ab1d5832371cb3c3b676
Noznoc <juliaconzon@gmail.com>
2017-04-26 19:22:24 -0400

    Fixed url

--- src/page-toggle.js
+++ src/page-toggle.js
@@ -4,7 +4,7 @@ function pageToggle(){
 	// activate active page
 	$(".navigation li").on("click", function(){
 		var section = $(this).attr('value');
-		window.location.href += "#" + section;
+		window.location.href = "https://noznoc.github.io/con-zone/index.html/#" + section;
 		var selected = "." + $(this).attr('value'); // get value of li to know what page to open
 		$(".navigation li").removeClass("active-page"); // clear active page
 		$(this).addClass("active-page"); // assign active page
@@ -18,22 +18,19 @@ function pageToggle(){
 	var url = window.location.href; // stores the url the client has entered
 	var id = url.substr(url.indexOf("#") + 1);
 	console.log(id)
-	if (id == "https://noznoc.github.io/con-zone/index.html") {
+	if (id.includes("noznoc")) {
 		id = "home";
 	}
-	if (id.length > 0){
-		$(".navigation li").removeClass("active-page"); // clear active page
-		console.log($(".navigation li").attr("value"))
-		$(".navigation li").each(function(){
-			if ($(this).attr("value") == id) {
-				$(this).addClass("active-page"); // assign active page
-			}
-		});
-		$(".body").find("div").hide(); // hide all pages
-		var selected = "." + id;
-		$(selected).show().children().show(); // show active page
-		if ($(".navigation li").attr("value") == "home") {
-			slideshow()
+	$(".navigation li").removeClass("active-page"); // clear active page
+	$(".navigation li").each(function(){
+		if ($(this).attr("value") == id) {
+			$(this).addClass("active-page"); // assign active page
 		}
+	});
+	$(".body").find("div").hide(); // hide all pages
+	var selected = "." + id;
+	$(selected).show().children().show(); // show active page
+	if ($(".navigation li").attr("value") == "home") {
+		slideshow()
 	}
 }
